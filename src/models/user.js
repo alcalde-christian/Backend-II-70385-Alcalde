c79fa4ac5bd1849cd494f4f2cb24e5c8158e1f70
Christian Alcalde <alcalde_christian@hotmail.com>
2025-01-17 20:05:29 -0300

    userModel created

---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+})
+
+const userModel = model("users", userSchema)
+
+export default userModel
